eae4768cfe1d4e4e6df329c3be1dc261333da027
Nicci Sheets <niccisheets@aol.com>
2018-06-25 14:30:59 -0400

    passed test for 2 matches returning 2

--- bash.rb
+++ bash.rb
@@ -1,5 +1,13 @@
 def number_comparison(my_ticket, winning_ticket)
-	1
+	my_ticket = my_ticket.split('')
+	winning_ticket = winning_ticket.split('')
+	matching_number = 0
+	winning_ticket.each_with_index do |num, index|
+		if num == my_ticket[index]
+			matching_number += 1
+		end
+	end
+	matching_number
 end
 
 
