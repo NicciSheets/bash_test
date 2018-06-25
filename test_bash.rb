f3306df85ae0a889f6e52f17a791522100ddbf77
Nicci Sheets <niccisheets@aol.com>
2018-06-25 14:14:17 -0400

    failed test for 2 matches

--- test_bash.rb
+++ test_bash.rb
@@ -4,6 +4,12 @@ require_relative "bash.rb"
 class TestTddBash < Minitest::Test
 
 	def test_assert_1_match_returns_1
-	assert_equal(1, number_comparison("1111", "1222"))		
+		assert_equal(1, number_comparison("1111", "1222"))		
 	end	
+
+	def test_assert_2_matches_returns_2
+		assert_equal(2, number_comparison("1111", "1122"))
+	end
+
 end
+
