071ea7d53e8e75d12b8dfb6b7b757a32e9fd1088
Nicci Sheets <niccisheets@aol.com>
2018-06-25 15:48:43 -0400

    printed out the new split array, num and index

--- bash.rb
+++ bash.rb
@@ -1,8 +1,11 @@
 def number_comparison(my_ticket, winning_ticket)
 	my_ticket = my_ticket.split('')
+	puts "my ticket is #{my_ticket}"
 	winning_ticket = winning_ticket.split('')
+	puts "winning ticket is #{winning_ticket}"
 	matching_number = 0
 	winning_ticket.each_with_index do |num, index|
+		puts "number is #{num} and index is #{index}"
 		if num == my_ticket[index]
 			matching_number += 1
 		end
