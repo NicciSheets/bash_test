a7f6cc0620b9d9174c305279039a09973a3032f8
Nicci Sheets <niccisheets@aol.com>
2018-06-26 18:18:43 -0400

    working on failed test for returning how many off from winner

--- test_bash.rb
+++ test_bash.rb
@@ -3,18 +3,18 @@ require_relative "bash.rb"
 
 class TestTddBash < Minitest::Test
 
-	def test_assert_1_match_returns_1
-		assert_equal(1, number_comparison("1222", "1111"))		
-	end	
+#	def test_assert_1_match_returns_1
+#		assert_equal(1, number_comparison("1222", "1111"))		
+#	end	
 
-	def test_assert_2_matches_returns_2
-		assert_equal(2, number_comparison("1122", "1111"))
+#	def test_assert_2_matches_returns_2
+#		assert_equal(2, number_comparison("1122", "1111"))
+#	end
+
+	def test_assert_3_away_from_winner_returns_3_off
+		assert_equal(3, number_comparison("1222", "1111"))
 	end
 
-	def test_assert_1_away_from_winner_returns_1_off
-		assert_equal("You are one off!", number_comparison("1222", "1111"))
-	end
-	
 	#def test_assert_end_result_is_array
 	#	my_tickets = ["1111", "1112", "1122"]
 	#	winning_tickets = ["1234", "2222"]
